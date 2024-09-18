e069a164301f73811999be65f49df5532c6acaf9
Kitronik Ltd <25480360+KitronikLtd@users.noreply.github.com>
2024-09-18 16:56:10 +0100

    Update shims.d.ts

--- shims.d.ts
+++ shims.d.ts
@@ -1,4 +1,7 @@
 // shims.d.ts
+// This file exposes the C++ functions to TypeScript for use in MakeCode.
+// It declares the setCustomPinHigh() and setCustomPinLow() functions so they can be called from your TypeScript code.
+
 //% shim=setCustomPinHigh
 declare function setCustomPinHigh(): void;
 
