e4b4e3879cdca917cc4958b9a52d646c0eaf9ddd
Kitronik Ltd <25480360+KitronikLtd@users.noreply.github.com>
2024-09-18 17:15:59 +0100

    Update customPinControl.cpp

--- customPinControl.cpp
+++ customPinControl.cpp
@@ -1,11 +1,17 @@
 #include "MicroBit.h"
 
-MicroBit uBit;
+namespace Craft_Code {
+    /**
+     * Set pin P0 to high without flickering.
+     */
+    void setCustomPinHigh() {
+        uBit.io.P0.setDigitalValue(1); // Set pin P0 high
+    }
 
-void setCustomPinHigh() {
-    uBit.io.P0.setDigitalValue(1); // Set pin P0 high
-}
-
-void setCustomPinLow() {
-    uBit.io.P0.setDigitalValue(0); // Set pin P0 low
+    /**
+     * Set pin P0 to low without flickering.
+     */
+    void setCustomPinLow() {
+        uBit.io.P0.setDigitalValue(0); // Set pin P0 low
+    }
 }
