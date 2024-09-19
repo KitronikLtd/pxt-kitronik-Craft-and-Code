244fbb27f062a77268069e9e01f80def61b77446
Kitronik Ltd <25480360+KitronikLtd@users.noreply.github.com>
2024-09-19 11:53:31 +0100

    Update customPinControl.ts

--- customPinControl.ts
+++ customPinControl.ts
@@ -1,11 +1,12 @@
 // Add your code here
 // customPinControl.ts
 // Simulates the pin control behavior without flickering.
+namespace Craft_Code{
+    function setCustomPinHigh(): void {
+        setCustomPinHigh()
+    }
 
-function setCustomPinHigh(): void {
-    setCustomPinHigh()
-}
-
-function setCustomPinLow(): void {
-    setCustomPinLow()
+    function setCustomPinLow(): void {
+        setCustomPinLow()
+    }
 }
