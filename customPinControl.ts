bfc05b00421f957fb05e01865d8abf7f07084dce
Kitronik Ltd <25480360+KitronikLtd@users.noreply.github.com>
2024-09-23 14:48:28 +0100

    Update customPinControl.ts

--- customPinControl.ts
+++ customPinControl.ts
@@ -2,9 +2,10 @@
 // Simulates the pin control behavior without flickering.
 
 namespace Craft_Code {
+    //% shim=Craft_Code::setCustomPinHigh
     export function setCustomPinHigh(): void {
     }
-
+    //% shim=Craft_Code::setCustomPinLow
     export function setCustomPinLow(): void {
     }
  
