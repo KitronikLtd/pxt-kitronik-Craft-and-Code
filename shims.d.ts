bfe70f712d31321b91031e34e9886f1780e1423e
Kitronik Ltd <25480360+KitronikLtd@users.noreply.github.com>
2024-09-27 13:39:41 +0100

    Update shims.d.ts

--- shims.d.ts
+++ shims.d.ts
@@ -4,14 +4,14 @@ declare namespace Craft_Code {
     /**
      * Set pin P0 to high without flickering.
      */
-    //% shim=Craft_Code::setCustomPinHigh
+    
     function setCustomPinHigh(): void;
 
     /**
      * Set pin P0 to low without flickering.
      */
-    //% shim=Craft_Code::setCustomPinLow
+
     function setCustomPinLow(): void;
 }
 
-// Auto-generated. Do not edit. Really.
+
