e47790444dc1a5c6ee910fff2147337ae65c5b04
Kitronik Ltd <25480360+KitronikLtd@users.noreply.github.com>
2024-09-27 14:17:03 +0100

    Update shims.d.ts, customPinControl.cpp

--- customPinControl.cpp
+++ customPinControl.cpp
@@ -5,7 +5,7 @@ namespace Craft_Code {
     /**
      * Set pin P0 to high without flickering.
      */
-    //% shim=Craft_Code::setCustomPinHigh
+    //% 
     void setCustomPinHigh() {
         uBit.io.P0.setDigitalValue(1);
     }
@@ -13,7 +13,7 @@ namespace Craft_Code {
     /**
      * Set pin P0 to low without flickering.
      */
-    //% shim=Craft_Code::setCustomPinLow
+    //% 
     void setCustomPinLow() {
         uBit.io.P0.setDigitalValue(0);
     }
