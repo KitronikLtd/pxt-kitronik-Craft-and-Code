d82421182ae8a204da9224719e7274daef7f4576
Kitronik Ltd <25480360+KitronikLtd@users.noreply.github.com>
2024-09-27 14:09:37 +0100

    Update shims.d.ts, customPinControl.cpp

--- customPinControl.cpp
+++ customPinControl.cpp
@@ -1,12 +1,18 @@
 #include "pxt.h"
-#include "MicroBit.h"
+using namespace pxt;
 
 namespace Craft_Code {
+    /**
+     * Set pin P0 to high without flickering.
+     */
     //% shim=Craft_Code::setCustomPinHigh
     void setCustomPinHigh() {
         uBit.io.P0.setDigitalValue(1);
     }
 
+    /**
+     * Set pin P0 to low without flickering.
+     */
     //% shim=Craft_Code::setCustomPinLow
     void setCustomPinLow() {
         uBit.io.P0.setDigitalValue(0);
