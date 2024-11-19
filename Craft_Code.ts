9f8eae8b050d026c96689c4634b357b5cb2b4ecc
JakeAtKitronik <164208666+JakeAtKitronik@users.noreply.github.com>
2024-11-19 11:42:46 +0000

    Update Craft_Code.ts

--- Craft_Code.ts
+++ Craft_Code.ts
@@ -5,7 +5,7 @@
 // subcategory=["More"]
 //% group = '["Bulb", "Motor", "Servo", "Touchpad"]'
 
-namespace Craft_Code {
+namespace CraftAndCode {
     let lastBulbBrightness: number = 100;
     let lastMotorSpeed: number = 350; //initial speed of the motor - currently set to 350 = "slow"
     let isBulbOn: boolean = false;
@@ -14,7 +14,7 @@ namespace Craft_Code {
     /**
      * Turn Bulb ON and OFF
      */
-    export enum bulbState {
+    export enum BULBSTATE {
         //% block="on"
         On,
         //% block="off"
@@ -24,7 +24,7 @@ namespace Craft_Code {
     /**
      * Turn motor ON and OFF
      */
-    export enum MotorState {
+    export enum MOTORSTATE {
         //% block="on"
         On,
         //% block="off"
@@ -34,7 +34,7 @@ namespace Craft_Code {
     /**
      * Set light brightness
      */
-    export enum BulbBrightnessState {
+    export enum BULBBRIGHTNESSSTATE {
         //% block="dim"
         Dim = 33,
         //% block="medium"
@@ -46,7 +46,7 @@ namespace Craft_Code {
     /**
      * Set motor speed
      */
-    export enum MotorSpeedState {
+    export enum MOTORSPEEDSTATE {
         //% block="slow"
         Slow = 350,
         //% block="medium"
@@ -60,19 +60,20 @@ namespace Craft_Code {
     ////////////////
 
     /**
-     * Turns on BULB  
+     * turn bulb [bulbstate]: Turns the craft and code bulb output on or off, as defined by a drop down menu
+     * @param bulbstate : on - turns the bulb output on. off - turns the bulb output off.
      */
-    //% blockId=Craft_and_Code_TurnBulb
+    //% blockId=craft_and_code_turnbulb
     //% block="turn bulb |%bulbstate||"
     //% weight=100 blockGap=8
     //% color=#770c67
     //% group="Bulb"
     //% x.min=0 x.max=1
     //% x.fieldOptions.precision=1
-    export function TurnBulb(bulbstate: bulbState): void {
-        isBulbOn = bulbstate === bulbState.On;
+    export function TurnBulb(bulbstate: BULBSTATE): void {
+        isBulbOn = bulbstate === BULBSTATE.On;
         if (isBulbOn) {
-            if (lastBulbBrightness == BulbBrightnessState.Bright) {
+            if (lastBulbBrightness == BULBBRIGHTNESSSTATE.Bright) {
                 // Use digital write for 100%
                 pins.digitalWritePin(DigitalPin.P8, 1);
             }
@@ -95,16 +96,17 @@ namespace Craft_Code {
     ////////////////////////
 
     /**
-     *  Set Bulb brightness
+     * Set Bulb brightness [brightnessstate]: changes the brightness of the attached bulb to three possible states, defined by a drop down menu.
+     * @param brightnessstate : "dim", the lowest possible brightness for the bulb. "medium", the midpoint between the lowest and highest brightness. "bright", the brightest possible setting for the bulb. 
      */
     //% subcategory="more"
-    //% blockId=Craft_and_Code_SetBulbBrightness
+    //% blockId=craft_and_code_setbulbbrightness
     //% block="set bulb brightness |%brightnessstate|"
     //% weight=100 blockGap=8
     //% color=#770c67
     //% group="Bulb"
 
-    export function SetBulbBrightness(brightnessstate: BulbBrightnessState): void {
+    export function SetBulbBrightness(brightnessstate: BULBBRIGHTNESSSTATE): void {
         // Always store the last value, but do not calculate or apply it yet
         lastBulbBrightness = brightnessstate as number;  // Store the last set value
         if (isBulbOn) { // Check if the Bulb is on before setting brightness
@@ -119,17 +121,18 @@ namespace Craft_Code {
     ////////////////
 
     /**
-     * Turns on Motor
+     * turn motor [motorstate]: Turns the craft and code motor output on or off, as defined by a drop down menu.
+     * @param motorstate : on - turns the motor output on. off - turns the motor output off. 
      */
-    //% blockId=Craft_and_Code_TurnMotor
+    //% blockId=craft_and_code_turnmotor
     //% block="turn motor |%motorstate||"
     //% weight=100 blockGap=8
     //% color=#770c67
     //% group="Motor"
     //% y.min=0 y.max=1
     //% y.fieldOptions.precision=1
-    export function TurnMotor(motorstate: MotorState): void {
-        if (motorstate === MotorState.On && !isMotorOn) {
+    export function TurnMotor(motorstate: MOTORSTATE): void {
+        if (motorstate === MOTORSTATE.On && !isMotorOn) {
             isMotorOn = true;
             // Gradually increase the motor speed to last set speed
             let totalDuration = 500; // Total duration in milliseconds to reach desired speed.
@@ -144,7 +147,7 @@ namespace Craft_Code {
                 basic.pause(pauseDuration);
             }
 
-        } else if (motorstate === MotorState.Off && isMotorOn) {
+        } else if (motorstate === MOTORSTATE.Off && isMotorOn) {
             isMotorOn = false;
 
             pins.analogWritePin(AnalogPin.P12, 0);
@@ -158,15 +161,17 @@ namespace Craft_Code {
     ////////////////
 
     /**
-     * Set motor speed
+     * Set motor speed [speedstate]:
+     * Changes the speed of a DC motor connected to the craft and code motor pads to one of three possible states, defined by a drop down menu.
+     * @param speedstate : "slow", the slowest possible speed for the attached motor to run at. "medium", the midpoint between the slowest and fastest the attached motor can run. "fast", the fastest possible speed for the attached motor. 
      */
     //% subcategory="more"
-    //% blockId=Craft_and_Code_SetMotorSpeed
+    //% blockId=craft_and_code_setmotorspeed
     //% block="set motor speed |%speedstate|"
     //% weight=100 blockGap=8
     //% color=#770c67
     //% group="Motor"
-    export function SetMotorSpeed(speedstate: MotorSpeedState): void {
+    export function SetMotorSpeed(speedstate: MOTORSPEEDSTATE): void {
         // Always store the last value
         let newSpeed = speedstate as number;
 
@@ -198,9 +203,10 @@ namespace Craft_Code {
     //////////////
 
     /**
-     * Set Servo Angle
+     * set servo [degree] degrees: Moves an attached servo motor connected to the craft and code board on the servo connector to the requested angle, defined by tbe parameter "degrees".
+     * @param degrees : how many degrees the attached servo motor needs to turn, for example, 90° 
      */
-    //% blockId=Craft_and_Code_SetServoAngle
+    //% blockId=craft_and_code_setservoangle
     //% block="set servo angle |%degrees| degrees"
     //% color=#770c67
     //% degrees.min=0 degrees.max=180
@@ -208,7 +214,7 @@ namespace Craft_Code {
     //% degrees.shadow="protractorPicker"
     //% weight=100 blockGap=8
     //% group="Servo"
-    export function setServoAngle(degrees: number) {
+    export function SetServoAngle(degrees: number) {
         // Check if the degrees are greater than 180
         if (degrees > 180 || degrees < 0) {
             console.error("Error: Angle cannot be more than 180 degrees or less then 0");
@@ -227,42 +233,47 @@ namespace Craft_Code {
 
 
     /**
-     * Wait for Touchpad
+     * wait for touchPad:
+     * The program will wait in an infinite loop, until there is a noted state change on the craft and code boards touchpad.
+     * I.E, the craft and code touchpad is pressed by a finger.
      */
     //% subcategory="more"
     //% block="wait for touchpad"
-    //% blockId=Craft_and_Code_Wait_for_touchpad
+    //% blockId=craft_and_code_wait_for_touchpad
     //% weight=100 blockGap=8
     //% color=#770c67
     //% group="Touchpad"
-    export function waitForTouchPad(): void {
+    export function WaitForTouchPad(): void {
 
         let touchPadState = false; // Initially, the touchpad is not pressed
 
         // Now, wait for the touchpad to be pressed
         while (!touchPadState) {
-            touchPadState = getTouchPadState(); // Reading the touchpad state only here
+            touchPadState = CurrentTouchPadState(); // Reading the touchpad state only here
             basic.pause(1);
 
         }
         while (touchPadState) {
-            touchPadState = getTouchPadState();
+            touchPadState = CurrentTouchPadState();
             basic.pause(1);
         }
     }
     let lastAverageReading = 0;
     let firstSample = true;
     /**
-     * Get TouchPad State
+     * touchpad pressed: 
+     * Returns the state of the craft and code touchpad as a boolean (1 or 0) value.
+     * When the craft and code touchpad is pressed by a finger, a boolean 1 is returned. 
+     * When the craft and code touchpad is not pressed, a boolean 0 is returned.
      */
     //% subcategory="more"
     //% block="touchpad pressed"
-    //% blockId=Touchpad pressed
+    //% blockId=craft_and_code_touchpad_pressed
     //% weight=100 blockGap=8
     //% color=#770c67
     //% group="Touchpad"
-    export function getTouchPadState(): boolean {
-        //"get TouchPadState" is effectively a bitbashed Capacitive Touch Sensor, which can run on a V1 or V2 Micro:bit
+    export function CurrentTouchPadState(): boolean {
+        //"Current TouchPadState" is effectively a bitbashed Capacitive Touch Sensor, which can run on a V1 or V2 Micro:bit
         //the method of operation is to take the IO pin high digitally, wait a millisecond, and then read the voltage on the pin
         //if a human finger is present, the decay of the voltage on the pin is much quicker than if no finger is present 
         //the average difference between no finger and a finger present is approximately a 5% increase in the readings.
@@ -280,7 +291,7 @@ namespace Craft_Code {
         let isTouched = false;
         //We take 400 samples in 40ms, and take an average of the reading - this is to average out mains hum at 50/60hz
         while (input.runningTime() - startTime_1 < 40) {
-            setCustomPinHigh(); //SetCustomPinHigh and setCustomPinLow call a C function (described in customPinControl.ts and customPinControl.cpp)
+            SetCustomPinHigh(); //SetCustomPinHigh and SetCustomPinLow call a C function (described in customPinControl.ts and customPinControl.cpp)
             control.waitMicros(100) // wait 100 MicroSeconds to read the data halfway through the curve  
             currentReading = pins.analogReadPin(AnalogPin.P0) //read the voltage on the pin - this should be at the midpoint of the decay curve due to the previous 1ms delay
             samplesTotal += currentReading // Add current reading to reading total, so we can average it later
@@ -288,7 +299,7 @@ namespace Craft_Code {
         }
 
         //force the pin low, to drain the rest of the voltage stored on the pin 
-        setCustomPinLow();
+        SetCustomPinLow();
 
 
         let currentAverageReading = samplesTotal / sampleCount; // Calculate the average of the samples taken within 40ms
@@ -313,9 +324,12 @@ namespace Craft_Code {
     ////SWITCH///////
     ////////////////
     /**
-     * Switch state
+     * switch closed:
+     * Returns the state of the craft and code switch pads as a boolean (1 or 0) value.
+     * When a switch connected to the craft and code switch pads is closed, a boolean 1 is returned. 
+     * When a switch connected to the craft and code switch pads is open, a boolean 0 is returned.
      */
-    //% blockId=Craft_and_Code_SwitchState
+    //% blockId=craft_and_code_switchstate
     //% block="switch closed"
     //% weight=100 blockGap=8
     //% color=#770c67
